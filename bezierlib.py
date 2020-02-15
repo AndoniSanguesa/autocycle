9835ad9ffde55bf7de55183d76e309dbc40c6281
andoniesanguesa <andoniesanguesa@gmail.com>
2020-02-15 15:18:35 -0500

    Citations and Bezier Plot Testing

--- bezierlib.py
+++ bezierlib.py
@@ -1,4 +1,34 @@
+import bezier
+import seaborn
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib
+
+nodes1 = np.asfortranarray([
+    [0.0, 0.5, 1.0],
+    [0.0, 1.0, 0.0],
+])
+
+curve1 = bezier.Curve(nodes1, degree=2)
+
+nodes2 = np.asfortranarray([
+    [0.0, 0.25, 0.5, 0.75, 1.0],
+    [0.0, 2.0, -2.0, 2.0, 0.0],
+])
+
+curve2 = bezier.Curve.from_nodes(nodes2)
+intersections = curve1.intersect(curve2)
+s_vals = np.asfortranarray(intersections[0, :])
+points = curve1.evaluate_multi(s_vals)
+
+seaborn.set()
+
+ax = curve1.plot(num_pts=256)
+_ = curve2.plot(num_pts=256, ax=ax)
+lines = ax.draw(
+    points[0, :], points[1, :],
+    marker="o", linestyle="None", color="black")
+_ = ax.axis("scaled")
+_ = ax.set_xlim(-0.125, 1.125)
+_ = ax.set_ylim(-0.0625, 0.625)
 
 
