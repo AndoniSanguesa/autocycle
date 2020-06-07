a51735aa68433aef30ad6d022015925feb1359c1
AndoniSanguesa <31708881+AndoniSanguesa@users.noreply.github.com>
2020-06-07 16:30:44 -0700

    Add files via upload

---
+++ Obstacle.py
@@ -0,0 +1,90 @@
+import numpy as np
+import Assistant
+import math
+
+
+
+class Obstacle:
+    def __init__(self, dist_to_edge, edge_to_path, edge_len):
+        self.dist_to_edge = dist_to_edge
+        self.edge_to_path = edge_to_path
+        self.edge_len = edge_len
+        self.edge_points = []
+        self.control_points = []
+        self.shown = False
+        if edge_to_path <= edge_len/2:
+            self.side = 1
+        else:
+            self.side = -1
+            self.edge_to_path = edge_len-edge_to_path
+        self.gamma = math.degrees(math.atan(self.edge_to_path / self.dist_to_edge)) + 5
+
+
+        self.calculate_obst_points()
+        self.calculate_control_point()
+
+    def adjust_gamma(self):
+        self.gamma += 5
+        self.calculate_control_point()
+
+    # Calculates the end points of the obstacle
+    def calculate_obst_points(self):
+        self.edge_points.clear()
+        x = self.dist_to_edge
+        y1 = self.side*self.edge_to_path
+        y2 = self.side*(self.edge_to_path-self.edge_len)
+        self.edge_points.append([x, x])
+
+        # This is structured this way so that the top point is always the first in the list
+        if self.side == 1:
+            self.edge_points.append([y1, y2])
+        else:
+            self.edge_points.append([y2, y1])
+
+    # Calculates the control points associated with this obstacle
+    def calculate_control_point(self):
+        self.control_points.clear()
+        angle_rad = np.deg2rad(self.gamma*self.side)
+        x = self.dist_to_edge
+        y = np.tan(angle_rad)*x
+
+        self.control_points.append([x, y])
+        self.control_points.append([x-(0.5*self.side), y-(0.5*self.side)])
+        self.control_points.append([x+(0.5*self.side), y - (0.5*self.side)])
+        # self.control_points.append([self.edge_points[0][ref_point], self.edge_points[1][ref_point]])
+
+    # Determines if a set of x and y coordinates at any point intersect with the obstacle
+    def intersect(self, xs, ys):
+        close_index = Assistant.find_closest_x(xs, self.dist_to_edge)
+        close_y = ys[close_index]
+        thisys = self.edge_points[1]
+        if thisys[0]-0.1 < close_y < thisys[1]+0.1 or thisys[1]-0.1 < close_y < thisys[0]+0.1:
+            self.shown = True
+            return True
+
+        return False
+
+    # Determines which end point of the obstacle is closest to the provided x and y value
+    def next_side(self, x, y):
+        diff_s1 = (self.edge_points[0][0]-x)**2 + (self.edge_points[1][0]-y)**2
+        diff_s2 = (self.edge_points[0][1]-x)**2 + (self.edge_points[1][1]-y)**2
+        if diff_s1 <= diff_s2:
+            self.side = 1
+        else:
+            self.side = -1
+        self.calculate_control_point()
+
+    def get_obst_points(self):
+        return self.edge_points
+
+    def get_control_points(self):
+        return self.control_points
+
+    def get_dist_to_edge(self):
+        return self.dist_to_edge
+
+    def get_edge_to_path(self):
+        return self.edge_to_path
+
+    def get_edge_len(self):
+        return self.edge_len
---
+++ bezier_visualization.py
@@ -0,0 +1,168 @@
+import bezier
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from pip._vendor.distlib.compat import raw_input
+from Assistant import CurveAssistant
+import sys
+from io import StringIO
+import time
+
+writefile = open("WriteData.py")
+exec("writefile")
+
+## OPEN TESTDATA FILE. ONLY FOR TESTING UNTIL INTEGRATION ##
+datafile = open('TestData', 'r')
+lines = datafile.readlines()
+stdin = sys.stdin
+
+time0 = time.time()
+fontP = FontProperties()
+fontP.set_size('small')
+labels = []
+# Distance to the end of the graph (our max viewing distance)
+sys.stdin = StringIO(lines.pop(0))
+end_dist = int(input())
+# Distances from the bike to the objects
+dist_to_edge = []
+# Shortest distance from the edges of the objects to the global path
+edge_to_path = []
+# Lengths of the objects
+edge_len = []
+
+# The final x and y values of the Bezier plot
+x_vals = []
+y_vals = []
+
+# The resolution dictates how many points are calculated (the lower the better, but slower)
+resolution = 0.05
+
+
+def reset_data():
+    dist_to_edge.clear()
+    edge_to_path.clear()
+    edge_len.clear()
+
+
+def get_data():
+    reset_data()
+    # Format is numbers with spaces inbetween:
+    # num_obst gamma1 dist_to_edge1 edge_to_path1 side_small_edge1 edge_len1 gamma2 ...
+    sys.stdin = StringIO(lines.pop(0))
+    data = raw_input().split()
+    for x in range(int(data[0])):
+        dist_to_edge.append(float(data[x * 3 + 1]))
+        edge_to_path.append(float(data[x * 3 + 2]))
+        edge_len.append(float(data[x * 3 + 3]))
+
+
+# Creates a CurveAssistant that will allow us to access our data
+curveas = CurveAssistant(end_dist)
+
+# The coordinate of the final control point at the end of the graph
+lastpoint = curveas.get_last_control_point()
+
+def plot():
+    for obstacle in curveas.obstacles:
+        # Converts the x-y points into the nt points
+        points = obstacle.edge_points
+        nt_1 = curveas.convert_nt(points[0][0], points[1][0])
+        nt_2 = curveas.convert_nt(points[0][1], points[1][1])
+        nt_obst = [[nt_1[0], nt_2[0]], [nt_1[1], nt_2[1]]]
+        plt.plot(nt_obst[0], nt_obst[1], linewidth=4)
+
+    # Plots and displays bezier curve
+    nt_last_point = curveas.convert_nt(lastpoint[0], lastpoint[1])
+    plt.plot([0, nt_last_point[0]], [0, nt_last_point[1]], color='green', linestyle='dashed')
+    nt_bez_list = [[], []]
+    for index in range(len(x_vals)):
+        # Converts xy points to nt points for the bezier curve
+        nt_bez_cur = curveas.convert_nt(x_vals[index], y_vals[index])
+        nt_bez_list[0].append(nt_bez_cur[0])
+        nt_bez_list[1].append(nt_bez_cur[1])
+
+    # Plots Bezier Curve
+    plt.plot(nt_bez_list[0], nt_bez_list[1])
+    # Plots Control points after converting from xy to nt
+    ctr_points = curveas.get_control_points()
+    ind = 0
+    for x in range(len(ctr_points)):
+        nt_ctr = curveas.convert_nt(ctr_points[x][0], ctr_points[x][1])
+        plt.plot(nt_ctr[0], nt_ctr[1], 'ro')
+        ind += 1
+    labels.extend(['Global Path', 'Actual Path', 'Control Point'])
+    plt.legend(labels, prop=fontP)
+
+    # Sets the plot axis to not be dumb
+    plt.axis('square')
+    plt.axis([0, nt_last_point[0], 0, nt_last_point[1]])
+    plt.show()
+    plt.clf()
+
+# Calculates x and y points for the bezier curve
+def calculate_curve():
+    # Empties the x and y values for the next curve
+    x_vals.clear()
+    y_vals.clear()
+
+    # The coordinates of the obstacles
+    curveas.compute_control_points()
+
+    # List of control points
+    nodes = curveas.get_fortran()
+
+    # This command creates a curve object which allows us to check the characteristics of the curve
+    curve = bezier.Curve(nodes, curveas.get_num_control_points() - 1)
+
+    # Variables keep track of x/y values and help in determining extrema
+    curpoint = [[0]]
+    index = 0
+    diff = [0, 0]
+    ys = [0, 0]
+
+    while curpoint[0][0] < end_dist:
+        curpoint = curve.evaluate(index * 1.00)
+        x = curpoint[0][0]
+        y = curpoint[1][0]
+
+        # To determine where the extrema occurs on the bezier curve
+        ys[0] = ys[1]
+        ys[1] = y
+        diff[0] = diff[1]
+        diff[1] = ys[1] - ys[0]
+        if diff[1] * diff[0] < 0:
+            curveas.extrema = [x, y]
+
+        # Adds x and y values to their respective array
+        x_vals.append(curpoint[0][0])
+        y_vals.append(curpoint[1][0])
+        index += resolution
+
+
+def create_environment():
+    labels.clear()
+    if len(lines) == 0:
+        return
+    get_data()
+    curveas.clear_obstacles()
+    # Creates objects
+    for x in range(len(edge_len)):
+        curveas.create_obstacle(dist_to_edge[x], edge_to_path[x], edge_len[x])
+    calculate_curve()
+    # Plots obstacles if they intersect the Bezier curve
+    ind = 1
+    for obstacle in curveas.obstacles:
+        # Allows for computation of control points if and only if the curve intersects the object
+        labels.append("Object " + str(ind))
+        obstacle.next_side(curveas.extrema[0], curveas.extrema[1])
+        print(obstacle.side)
+        while obstacle.intersect(x_vals, y_vals):
+            obstacle.adjust_gamma()
+            calculate_curve()
+        ind += 1
+    plot()
+    create_environment()
+
+create_environment()
+sys.stdin = stdin
+time1 = time.time()
+print(time1 - time0)
