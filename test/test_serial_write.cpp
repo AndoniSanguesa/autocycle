33ed8619078810fca441cafb8f9d309804419939
asangues <andoniesanguesa@gmail.com>
2021-02-20 20:33:06 +0000

    serial writing tests updated

--- test/test_serial_write.cpp
+++ test/test_serial_write.cpp
@@ -1,5 +1,9 @@
 #include <ros/ros.h>
 #include <serial/serial.h>
+#include <string>
+
+using namespace std;
+
 
 int main(int argc, char **argv){
     // Registers node with the master
@@ -7,18 +11,25 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
 
     // Creates serial object to write to
-    serial::Serial my_serial("/dev/ttyACM0", (long) 115200, serial::Timeout::simpleTimeout(0));
+    serial::Serial my_serial("/dev/ttyACM0", (long) 1, serial::Timeout::simpleTimeout(0));
 
     // String to repeat (that will hopefully be echoed
     string to_repeat = "Hello World.\n";
 
     // Current index for the byte we are printing
     int ind = 0;
+    uint8_t cur;
+    string read;
 
     // Prints string one byte at a time then receives the byte back and prints it
     while(ros::ok()){
-        my_serial.print(to_repeat[ind%to_repeat.length()]);
-        cout << my_serial.read();
+	cur = (uint8_t) to_repeat[ind%to_repeat.length()];
+        my_serial.write(&cur, 1);
+        while(read == ""){
+	    read.append(my_serial.read());
+        }
+	cout << read << flush;
+	read.clear();
         ind++;
     }
 }
