da2510f9f64095aeb108e81cd954846570e5dbe4
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-03-02 11:14:44 -0500

    added fake action sender for use off bike

---
+++ test/test_fake_send_action.cpp
@@ -0,0 +1,19 @@
+#include <ros/ros.h>
+#include <autocycle/Action.h>
+
+bool SendAction(
+        autocycle::Action::Request &req,
+        autocycle::Action::Response &resp
+
+){
+    return true;
+}
+
+int main(int argc, char **argv){
+    // Registers node with the master
+    ros::init(argc, argv, "test_fake_send_action");
+    ros::NodeHandle nh;
+
+    // Registers fake Service Server with master.
+    ros::ServiceServer act_srv = nh.advertiseService("send_action", &SendAction);
+}
