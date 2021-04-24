6af1b2d9427d82b6254c92074a6face29765a32c
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-04-23 20:36:10 -0400

    Fixed action sending simulator

--- test/test_fake_send_action.cpp
+++ test/test_fake_send_action.cpp
@@ -1,22 +1,64 @@
 #include <ros/ros.h>
-#include <autocycle_extras/Action.h>
+#include <serial/serial.h>
+#include <autocycle_extras/GetDelta.h>
+#include <std_msgs/Float32.h>
+#include <chrono>
+#include <std_srvs/Empty.h>
 
-bool SendAction(
-        autocycle_extras::Action::Request &req,
-        autocycle_extras::Action::Response &resp
+using namespace std;
 
+float dist_trav = 0;
+float velocity = 0;
+float roll = 0;
+float time_elapsed = 0;
+
+bool reset_distance(
+    std_srvs::Empty::Request &req,
+    std_srvs::Empty::Response &resp
 ){
-    
+    dist_trav = 0;
     return true;
 }
 
-int main(int argc, char **argv){
-    // Registers node with the master
-    ros::init(argc, argv, "test_fake_send_action");
+void update_velocity(const std_msgs::Float32 data){
+    velocity = data.data;
+}
+
+void update_roll(const std_msgs::Float32 data){
+    roll = data.data;
+}
+
+int main(int argc, char **argv) {
+    // Registers node with the master.
+    ros::init(argc, argv, "read_serial");
     ros::NodeHandle nh;
 
-    // Registers fake Service Server with master.
-    ros::ServiceServer act_srv = nh.advertiseService("send_action", &SendAction);
+    // Creates service proxy that will grab new deltas
+    ros::ServiceClient get_delta = nh.serviceClient<autocycle_extras::GetDelta>("get_delta");
+    autocycle_extras::GetDelta::Request req;
+    autocycle_extras::GetDelta::Response resp;
 
-    ros::spin();
+    // Creates Service that will reset the distance
+    ros::ServiceServer get_path = nh.advertiseService("reset_action", &reset_distance);
+
+    // Creates subscriber for updating velocity
+    ros::Subscriber get_vel = nh.subscribe("sensors/vel", 1, &update_velocity);
+
+    // Creates subscriber for updating roll
+    ros::Subscriber get_roll = nh.subscribe("sensors/roll", 1, &update_roll);
+
+    // Starts the clock!
+    auto start = std::chrono::high_resolution_clock::now();
+
+    while(ros::ok()){
+        ros::spinOnce();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        auto start = std::chrono::high_resolution_clock::now();
+        dist_trav += velocity * (((float) duration.count())/1000.0);
+        req.x = dist_trav;
+        req.roll = roll;
+        get_delta.call(req, resp);
+    }
+
 }
