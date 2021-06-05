73b2522e2824eea52e689c3c1ef71be9e0b7c2cd
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-06-05 10:54:10 -0700

    added rasppi ip getter

---
+++ get_ip.py
@@ -0,0 +1,11 @@
+from pythonping import ping
+result_list = []
+
+for i in range(256):
+    ip = f'192.168.{i}.221'
+    print(ip)
+    res = ping(ip, verbose=False, count=2, timeout=0.2)
+    if any([resp.success for resp in res._responses]):
+        result_list.append(ip)
+
+print(result_list)
