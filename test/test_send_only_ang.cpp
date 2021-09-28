99fa6ca90243f8eeee8bcbff22cfa8344fcaa7a8
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-09-28 10:38:22 -0400

    Revert "Revert "Merge branch 'change_deltas' of https://github.com/AndoniSanguesa/autocycle into change_deltas""
    This reverts commit c1ff59af55cd6f6554f007e56830c5a14df5b9ab.

--- test/test_send_only_ang.cpp
+++ test/test_send_only_ang.cpp
@@ -38,6 +38,10 @@ int main(int argc, char **argv) {
     ros::ServiceClient get_delta = nh.serviceClient<autocycle_extras::GetDelta>("get_delta");
     autocycle_extras::GetDelta::Request req;
     autocycle_extras::GetDelta::Response resp;
+
+    req.x = 0;
+    req.vel = 0;
+    resp.delta = -1;
 	
     ros::service::waitForService("get_delta");
     ros::service::waitForService("calc_deltas");
@@ -52,7 +56,12 @@ int main(int argc, char **argv) {
 
     // Starts the clock!
     auto start = std::chrono::high_resolution_clock::now();
-    
+
+    // Waits until a path is ready to be followed
+    while(resp.delta == -1){
+        get_delta.call(req, resp);
+    }
+
     // Rates the delta querying speed
     ros::Rate loop_rate(10);
 
