56b4bf0e7272b88fc211472be1fb5fea7f3737e2
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-10-13 15:07:02 -0400

    added gps coord subscriber to gps directions

--- src/autocycle_py/gps_directions.py
+++ src/autocycle_py/gps_directions.py
@@ -1,9 +1,23 @@
 import googlemaps
+# import rospy
+# from autocycle_extras.msg import GPS
 
 gmaps = googlemaps.Client(key="")
+cur_gps_coords = (0, 0)
+
+
+def update_gps(data):
+    global cur_gps_coords
+
+    cur_gps_coords = (data.long, data.lat)
 
 
 def get_gps_data():
+    # Initializes the ROS node
+    # rospy.init_node("calculate_deltas")
+
+    # Creates Subscriber for new paths to generate deltas for
+    # rospy.Subscriber("cycle/gps", GPS, update_gps)
+
     return
-    # code for getting data from Arduino
     # will likely only need a continuous stream of geo-coordinates
