20c7742e8cecf5bc646f020e0f86c4d9aaadd9be
Cooper Grill <coopergrill@gmail.com>
2021-10-13 14:48:49 -0400

    added file for gps directions

---
+++ src/autocycle_py/gps_directions.py
@@ -0,0 +1,9 @@
+import googlemaps
+
+gmaps = googlemaps.Client(key="")
+
+
+def get_gps_data():
+    return
+    # code for getting data from Arduino
+    # will likely only need a continuous stream of geo-coordinates
