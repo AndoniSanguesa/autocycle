37fed3bdc0358e83125a644bf6667b98bfcfb705
Andoni Sanguesa <andoniesanguesa@gmail.com>
2021-11-03 10:02:29 -0400

    added pre-experiment systems check

--- test/test_fake_send_action.cpp
+++ test/test_fake_send_action.cpp
@@ -1,31 +1,32 @@
 #include <ros/ros.h>
 #include <serial/serial.h>
 #include <autocycle_extras/GetDelta.h>
+#include <autocycle_extras/CalcDeltas.h>
 #include <std_msgs/Float32.h>
 #include <chrono>
 #include <std_srvs/Empty.h>
+#include <unistd.h>
 
 using namespace std;
 
+// Creates serial object to write to
+serial::Serial my_serial("/dev/ttyACM0", (long) 115200, serial::Timeout::simpleTimeout(0));
+
 float dist_trav = 0;
 float velocity = 0;
-float roll = 0;
+float state = 0;
 float time_elapsed = 0;
 
-bool reset_distance(
-    std_srvs::Empty::Request &req,
-    std_srvs::Empty::Response &resp
-){
+void reset_distance(const autocycle_extras::CalcDeltas data){
     dist_trav = 0;
-    return true;
 }
 
 void update_velocity(const std_msgs::Float32 data){
     velocity = data.data;
 }
 
-void update_roll(const std_msgs::Float32 data){
-    roll = data.data;
+void update_state(const std_msgs::Float32 data){
+    state = data.data;
 }
 
 int main(int argc, char **argv) {
@@ -33,31 +34,56 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "read_serial");
     ros::NodeHandle nh;
 
+    ros::service::waitForService("due_ready");
+    ros::service::waitForService("get_delta");
+
     // Creates service proxy that will grab new deltas
     ros::ServiceClient get_delta = nh.serviceClient<autocycle_extras::GetDelta>("get_delta");
     autocycle_extras::GetDelta::Request req;
     autocycle_extras::GetDelta::Response resp;
 
+    // Initializes delta requests and response. Used to wait for an actual delta
+    resp.delta = -1;
+    req.vel = 0;
+    req.x = 0;
+
     // Creates Service that will reset the distance
-    ros::ServiceServer get_path = nh.advertiseService("reset_action", &reset_distance);
+    ros::Subscriber get_path = nh.subscribe("cycle/calc_deltas", 1, &reset_distance);
 
     // Creates subscriber for updating velocity
     ros::Subscriber get_vel = nh.subscribe("sensors/vel", 1, &update_velocity);
 
-    // Creates subscriber for updating roll
-    ros::Subscriber get_roll = nh.subscribe("sensors/roll", 1, &update_roll);
+    // Creates subscriber for updating state
+    ros::Subscriber get_state = nh.subscribe("sensors/state", 1, &update_state);
 
     // Starts the clock!
-    auto start = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> end;
+    // Waits until a path is ready to be followed
+    while(resp.delta == -1){
+        get_delta.call(req, resp);
+    }
+
+    // Rates the delta querying speed
+    // ros::Rate loop_rate(10);
 
     while(ros::ok()){
+	// loop_rate.sleep();
         ros::spinOnce();
-        auto end = std::chrono::high_resolution_clock::now();
+
+        if(state!=0 && state!=3){
+            ROS_WARN_STREAM("state: " << state);
+	    ros::shutdown();
+        }
+
+        end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        auto start = std::chrono::high_resolution_clock::now();
+        start = std::chrono::high_resolution_clock::now();
         dist_trav += velocity * (((float) duration.count())/1000.0);
         req.x = dist_trav;
         req.vel = velocity;
         get_delta.call(req, resp);
+
+        usleep(250000);
     }
 }
